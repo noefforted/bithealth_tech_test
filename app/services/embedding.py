d9deaa106565910337288e7cb5935f391ec17d93
noefforted <ahmadopa73@gmail.com>
2026-02-01 18:49:59 +0700

    fake embed

--- app/services/embedding.py
+++ app/services/embedding.py
@@ -1,16 +1,15 @@
 from sentence_transformers import SentenceTransformer
-import torch
+import random
 
 class EmbeddingService:
     def __init__(self):
-        # Model ini menghasilkan 384 dimensi
-        self.model_name = 'paraphrase-multilingual-MiniLM-L12-v2'
-        self.model = SentenceTransformer(self.model_name)
+        # self.model_name = 'paraphrase-multilingual-MiniLM-L12-v2' # Model with 384 dimensions
+        # self.model = SentenceTransformer(self.model_name)
+        self.dimension = 384 # fake embedding dimension
         
     def get_embedding(self, text: str) -> list[float]:
-        """
-        Menghasilkan vektor semantik sungguhan menggunakan model AI.
-        """
-        # Menghasilkan list of floats
-        embedding = self.model.encode(text)
-        return embedding.tolist()
+        # # Menghasilkan list of floats
+        # embedding = self.model.encode(text)
+        # return embedding.tolist()
+        random.seed(abs(hash(text)) % 10000)
+        return [random.random() for _ in range(self.dimension)] # fake embedding
